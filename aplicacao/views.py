56ae243400555cee4599453bf213b8d4e2267f8c
Yuridrescher <yuridrescheryd@gmail.com>
2024-11-25 15:53:42 -0300

    Update views.py

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aplicacao/index.html')
+def index2(request):
+    return render(request, 'aplicacao/segunda.html')
