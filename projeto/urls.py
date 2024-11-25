ab9a1c42e00b51286fad87c2766aad5689d8d3f8
Yuridrescher <yuridrescheryd@gmail.com>
2024-11-25 15:54:32 -0300

    Update urls.py

--- projeto/urls.py
+++ projeto/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from projeto.settings import DEBUG
 from django.conf import settings
 from django.conf.urls.static import static
-from aplicacao.views import index
+from aplicacao.views import index, index2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('2', index2),
 ]
 
 
