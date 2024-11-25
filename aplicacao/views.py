50c53448928293e18de3e6726ab32f5fefe22aaf
Yuridrescher <yuridrescheryd@gmail.com>
2024-11-25 16:18:44 -0300

    Update views.py

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'aplicacao/index.html')
-def index2(request):
-    return render(request, 'aplicacao/segunda.html')
+
+def semana(request, semana):
+    context = {}
+    if semana == 'segunda':
+        context = {
+            'img': '/static/aplicacao/img.pmg',
+            'semana':'segunda-feira'
+        }
+
+        return render(request, 'aplicacao/segunda.html',context)
